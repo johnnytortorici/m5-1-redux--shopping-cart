b027deda6bd5b07709356998683f721b8378b1c1
johnnytortorici <johnny.tortorici@gmail.com>
2020-08-05 16:45:34 -0400

    Exercise 3A

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,15 @@ const initialState = {};
 
 export default function cartReducer(state = initialState, action) {
   switch (action.type) {
+    case "ADD_ITEM": {
+      return {
+        ...state,
+        [action.item.id]: {
+          ...action.item,
+          quantity: 1,
+        },
+      };
+    }
     default:
       return state;
   }
